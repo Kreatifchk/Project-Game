8295489ee7c1c04bf6c0826c157d95379c095256
User <User@Sergey>
2017-01-17 00:37:36 +0300

    -Доделан класс с окном для найма войск
    -В классе Resize добавлен метод возвращающий ImageIcon

--- Стратегия/src/strategy/ArmyTownHiring.java
+++ Стратегия/src/strategy/ArmyTownHiring.java
@@ -1,21 +1,177 @@
 package strategy;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
+import warriors.Archer;
+import warriors.Footman;
+
 @SuppressWarnings("serial")
-public class ArmyTownHiring extends JLabel {
+public class ArmyTownHiring extends JLabel implements MouseListener, ActionListener {
 	
 	String name;
 	int townId;
 	
+	boolean ent;
+	
+	ExitButton exit;
+	
+	ArmButton[] arm = new ArmButton[2];
+	
 	public ArmyTownHiring() {
-		setBounds(100, 50, 400, 500);
+		setSize(498, 470);
+		
+		exit = new ExitButton();
+		
 		setOpaque(true);
 		setBackground(new Color(150, 75, 0));
-		setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
+		setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 4));
+		setLocation((1000 - getWidth()) / 2, 25);
+		
+		add(exit);
+		armAdd();
+	}
+	
+	private void armAdd() {
+		arm[0] = new ArmButton(new Footman(0));
+		arm[1] = new ArmButton(new Archer(0));
+		
+		int x = 4, y = 49;
+		int size = 98;
+		for (int i = 0; i < arm.length; i++) {
+			//arm[i] = new ArmButton(new Footman(0));
+			arm[i].setBounds(x, y, size, size);
+			arm[i].addActionListener(this);
+			
+			arm[i].setIcon(Resize.resizeIcon(arm[i].ta.icon.getImage(), size, size));
+			add(arm[i]);
+			x += size;
+		}
+	}
+	
+	private void armDel() {
+		for (int i = 0; i < arm.length; i++) {
+			remove(arm[i]);
+			arm[i] = null;
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		for (int i = 0; i < arm.length; i++) {
+			if (e.getSource() == arm[i]) {
+				if (Game.town.get(townId).line.size() < 12) {
+					Game.town.get(townId).line.add(arm[i].ta);
+				}
+				repaint();
+				armDel();
+				armAdd();
+			}
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent a) {
+		if (a.getSource() == exit) {
+			Game.jlp.remove(this);
+			Game.jlp.repaint();
+		}
+	}
+	@Override
+	public void mouseEntered(MouseEvent a) {
+		if (a.getSource() == exit) {
+			ent = true;
+			exit.repaint();
+		}
+	}
+	@Override
+	public void mouseExited(MouseEvent a) {
+		if (a.getSource() == exit) {
+			ent = false;
+			exit.repaint();
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent a) {
+	}
+	@Override
+	public void mouseReleased(MouseEvent a) {
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(new Color(255, 255, 128));
+		setFont(new Font("Garamond", Font.BOLD, 25));
+		g2d.drawString(name, (getWidth() - name.length()*25) / 2, 30);
+		g2d.setColor(Color.BLACK);
+		g2d.setStroke(new BasicStroke(5));
+		int y = 46;
+		g2d.drawLine(4, y, getWidth()-4, y);
+		y = 345;
+		g2d.drawLine(4, y, getWidth()-4, y);
+		
+		//int x = 4;
+		int x = (getWidth() - 8 - 360) / 2;
+		y = 348;
+		for (int i = 0; i < Game.town.get(townId).line.size(); i++) {
+			if (i == 6) {
+				y += 60;
+				x = (getWidth() - 8 - 360) / 2;
+			}
+			Image img = Game.town.get(townId).line.get(i).icon.getImage();
+			img = Resize.resize(img, 60, 60);
+			g2d.drawImage(img, x, y, null);
+			
+			g2d.setColor(Color.BLACK);
+			g2d.drawOval(x + 20, y + 20, 20, 20);
+			g2d.fillOval(x + 20, y + 20, 20, 20);
+			g2d.setColor(Color.RED);
+			setFont(new Font("Garamond", Font.BOLD, 20));
+			g2d.drawString("" + (i+1), x + 24, y + 36);
+			
+			x += 60;
+		}
+	}
+	
+	private class ArmButton extends JButton {
+		TypeArmy ta;
+		public ArmButton(TypeArmy ta) {
+			this.ta = ta;
+		}
+	}
+	
+	private class ExitButton extends JButton {
+		public ExitButton() {
+			setBounds(ArmyTownHiring.this.getWidth()-44, 4, 40, 40);
+			setOpaque(false);
+			setBorder(null);
+			addMouseListener(ArmyTownHiring.this);
+		}
+		@Override
+		public void paintComponent(Graphics g) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setColor(Color.GRAY);
+			if (ent == true) {
+				g2d.fillRect(0, 0, getWidth(), getHeight());
+			}
+			g2d.setColor(Color.WHITE);
+			g2d.drawLine(0, 0, 40, 40);
+			g2d.drawLine(0, 40, 40, 0);
+		}
 	}
 
 }
--- Стратегия/src/strategy/Resize.java
+++ Стратегия/src/strategy/Resize.java
@@ -1,17 +1,30 @@
 package strategy;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
 
 public class Resize {
 	
-	public static java.awt.Image resize(java.awt.Image img, int width, int height) {
+	public static Image resize(Image img, int width, int height) {
 		java.awt.Image im;
 		BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
 		Graphics g = bi.getGraphics();
 		g.drawImage(img, 0, 0, null);
 		im = bi.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
 		return im;
+	}
+	
+	public static ImageIcon resizeIcon(Image img, int width, int height) {
+		Image im;
+		BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		Graphics g = bi.getGraphics();
+		g.drawImage(img, 0, 0, null);
+		im = bi.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon ii = new ImageIcon(im);
+		return ii;
 	}
 	
 }
