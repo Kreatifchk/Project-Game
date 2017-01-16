583140505e3bd6f6c222ee0de6e0bcfdfcd123b6
User <User@Sergey>
2017-01-16 22:53:23 +0300

    Update test

--- Стратегия/src/strategy/ArmyTownHiring.java
+++ Стратегия/src/strategy/ArmyTownHiring.java
@@ -1,5 +1,8 @@
 package strategy;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
@@ -7,5 +10,12 @@ public class ArmyTownHiring extends JLabel {
 	
 	String name;
 	int townId;
+	
+	public ArmyTownHiring() {
+		setBounds(100, 50, 400, 500);
+		setOpaque(true);
+		setBackground(new Color(150, 75, 0));
+		setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
+	}
 
 }
